bd0dea6de955928ee1f005f615cb1b6a7e5b5d5c
Resha228 <123998484+Resha228@users.noreply.github.com>
2023-10-20 15:13:53 +0300

    edit-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PhoneForm } from './Phonebook/PhoneForm';
 import { ListItem } from './Phonebook/FormList';
 import { nanoid } from 'nanoid';
 import { Container } from './App.style';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [contactFilter, setContactFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addItem = newItem => {
     const existingContact = contacts.find(
@@ -57,5 +63,3 @@ export const App = () => {
     </Container>
   );
 };
-
-export default App;
